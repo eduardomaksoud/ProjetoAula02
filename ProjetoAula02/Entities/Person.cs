0d4cb5ba4a4569995b76153735b418908c3d4cc9
Eduardo Maksoud <eduardobm@tecgraf.puc-rio.br>
2024-01-19 19:22:47 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAula02/Entities/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace ProjetoAula02.Entities
+{
+    /// <summary>
+    ///  Class to represent a person.
+    /// </summary>
+    public class Person
+    {
+        #region Attributes
+        public Guid? _id { get; set; }
+        public string? _name { get; set; }
+        #endregion
+
+        #region Properties
+        public Guid? Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+
+        public string? Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(value);
+
+                _name = value;
+            }
+            get { return _name; }
+        }
+        #endregion
+    }
+}
